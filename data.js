4e8c2066e31bbbc6dbf3270e6617e8b0a69308e6
gokhanpa-star <gokhanpa@gmail.com>
2026-03-30 15:52:12 +0300

    Add files via upload

--- data.js
+++ data.js
@@ -1,17 +1,53 @@
 window.APP_DATA = {
   "spirits": [
-    { "id": "sp_vodka", "name": "Vodka", "available": true },
-    { "id": "sp_rom", "name": "Rom", "available": true },
-    { "id": "sp_cin", "name": "Cin", "available": true },
-    { "id": "sp_tekila", "name": "Tekila", "available": false },
-    { "id": "sp_triplesec", "name": "Triple Sec", "available": false },
-    { "id": "sp_viski", "name": "Viski", "available": true }
+    {
+      "id": "sp_vodka",
+      "name": "Vodka",
+      "available": true
+    },
+    {
+      "id": "sp_rom",
+      "name": "Rom",
+      "available": true
+    },
+    {
+      "id": "sp_cin",
+      "name": "Cin",
+      "available": true
+    },
+    {
+      "id": "sp_tekila",
+      "name": "Tekila",
+      "available": false
+    },
+    {
+      "id": "sp_triplesec",
+      "name": "Triple Sec",
+      "available": true
+    },
+    {
+      "id": "sp_viski",
+      "name": "Viski",
+      "available": true
+    },
+    {
+      "id": "s8sjoea9",
+      "name": "Baileys",
+      "available": true
+    },
+    {
+      "id": "u6yvw461",
+      "name": "Kahlua",
+      "available": true
+    }
   ],
   "cocktails": [
     {
       "id": "ck_mojito",
       "name": "Mojito",
-      "spiritIds": ["sp_rom"],
+      "spiritIds": [
+        "sp_rom"
+      ],
       "recipe": "Rom ile hazırlanır, nane ve lime ile ferah servis edilir.",
       "category": "Ferah",
       "favorite": true
@@ -19,9 +55,23 @@ window.APP_DATA = {
     {
       "id": "ck_gintonic",
       "name": "Gin Tonic",
-      "spiritIds": ["sp_cin"],
+      "spiritIds": [
+        "sp_cin"
+      ],
       "recipe": "Cin ve tonik ile buzlu servis edilir.",
       "category": "Klasik",
+      "favorite": false
+    },
+    {
+      "id": "8agmbzyz",
+      "name": "B52",
+      "spiritIds": [
+        "sp_triplesec",
+        "s8sjoea9",
+        "u6yvw461"
+      ],
+      "recipe": "Shot bardağına",
+      "category": "Tatlı",
       "favorite": false
     }
   ]
